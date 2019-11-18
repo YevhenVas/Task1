97d21c98b11da8c84068b469bb2989feb017d418
Levskie <ievgenpet@gmail.com>
2019-11-18 19:58:16 +0200

    Initial commit

---
+++ src/main/java/Grow/Prime_Digit.java
@@ -0,0 +1,15 @@
+package Grow;
+
+public class Prime_Digit {
+    if (true){
+        int Prime=1;
+            for (int i=0; i<=5000; i++){
+                if (int balance>0){
+                    balance =
+
+                }
+            }
+
+        return Prime;
+    }
+}
